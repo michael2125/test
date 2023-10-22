af730854828a0672f1c2463bdb8f3d5cbd2da3eb
michael2125 <147800592+michael2125@users.noreply.github.com>
2023-10-22 21:33:14 +1100

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.io.File;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BackUp{
+        private String name;
+    private String email;
+    private String password;
+
+    public static String[] courseList = {
+        "software",
+        "dataframework",
+        "Computer Programming Fundamentals",
+        "Data Structures and Algorithms",
+        "Computer Architecture",
+        "Operating Systems",
+        "Database Management Systems",
+        "Network Programming",
+        "Network Security",
+        "Compiler Design",
+        "Artificial Intelligence",
+        "Machine Learning",
+        "Computer Graphics",
+        "Cloud Computing",
+        "Signal and System",
+        "Networking and Communication"
+    };
+    private static String filename = "customers.data";
+
+    public static void initialize() {
+        try{
+            List<Customer> customers = new LinkedList<>();
+            File file = new File(filename);
+            if (!file.exists()){
+                file.createNewFile();
+                FileOutputStream fileOut = new FileOutputStream(filename);
+                ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);            
+                objectOut.writeObject(customers);
+                objectOut.close();
+                fileOut.close();
+            }    
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BackUp.class.getName()).log(Level.SEVERE, "File Not Found", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BackUp.class.getName()).log(Level.SEVERE, "Reading Error", ex);
+        } 
+    }
+
+    public static List<Customer> read(){
+        List<Customer> customers = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+            customers = (List)objectIn.readObject();
+            objectIn.close();
+            fileIn.close();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BackUp.class.getName()).log(Level.SEVERE, "File Not Found", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BackUp.class.getName()).log(Level.SEVERE, "Reading Error", ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BackUp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return customers;
+    }
+
+    public static void write(List<Customer> customers){
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);            
+            objectOut.writeObject(customers);
+            objectOut.close();
+            fileOut.close();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BackUp.class.getName()).log(Level.SEVERE, "File Not Found", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BackUp.class.getName()).log(Level.SEVERE, "Reading Error", ex);
+        }
+    }
+}
+
+
+
+
+
+
+
+
